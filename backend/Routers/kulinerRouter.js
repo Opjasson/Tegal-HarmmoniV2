21a22947c463b7cbb6cc949843f928cbe880e3e7
Opjasson <azmitheusos333@gmail.com>
2025-07-12 20:01:32 +0700

    backend : add route update & delete kuliner

--- backend/Routers/kulinerRouter.js
+++ backend/Routers/kulinerRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { addData, getData, getDataById } from "../Controllers/kulinerController.js";
+import { addData, deleteKulinerById, getData, getDataById, updateKulinerById } from "../Controllers/kulinerController.js";
 
 const router = express.Router();
 
 router.get("/kuliner", getData);
 router.get("/kuliner/:id", getDataById);
 router.post("/kuliner", addData);
+router.patch("/kuliner/:id", updateKulinerById);
+router.delete("/kuliner/:id", deleteKulinerById);
 
 export default router;
