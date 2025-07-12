0d4ba4c01d6c9e31ecdf0c8a6efe65cd4e5000c8
Opjasson <azmitheusos333@gmail.com>
2025-07-12 19:49:36 +0700

    backend : add route delete hotel by ia

--- backend/Routers/hotelRouters.js
+++ backend/Routers/hotelRouters.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { addData, getData, getDataById, updateHotelById} from "../Controllers/hotelControllers.js"
+import { addData, deleteHotelById, getData, getDataById, updateHotelById} from "../Controllers/hotelControllers.js"
 
 
 const router = express.Router()
@@ -9,6 +9,7 @@ router.get("/hotel",getData)
 router.get("/hotel/:id", getDataById)
 router.post("/hotel", addData);
 router.patch("/hotel/:id", updateHotelById);
+router.delete("/hotel/:id", deleteHotelById);
 
 
 export default router;
