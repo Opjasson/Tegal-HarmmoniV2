26677823c3adc8ee34d26e12f4ac2810ef655fc5
Opjasson <azmitheusos333@gmail.com>
2025-07-12 20:02:43 +0700

    backend : add route update & delete wisata

--- backend/Routers/wisataRouters.js
+++ backend/Routers/wisataRouters.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { addData, getData, getDataById } from "../Controllers/wisataControllers.js";
+import { addData, deleteWisataById, getData, getDataById, updateWisataById } from "../Controllers/wisataControllers.js";
 
 const router = express.Router();
 
 router.get("/wisata", getData);
 router.get("/wisata/:id", getDataById);
 router.post("/wisata", addData);
+router.patch("/wisata/:id", updateWisataById);
+router.delete("/wisata/:id", deleteWisataById);
 
 export default router;
